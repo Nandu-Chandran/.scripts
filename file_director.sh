d11cf5742c223a6fef34fb11e8cf85e8ddef5471
Nandu Chandran <nanduchandran121@gmail.com>
2021-06-24 15:35:30 +0530

    add img path

--- file_director.sh
+++ file_director.sh
@@ -4,8 +4,10 @@ dir="/data/data/com.termux/files/home/storage/downloads/"
 doc_path="/data/data/com.termux/files/home/storage/shared/Documents/"
 music_path="/data/data/com.termux/files/home/storage/shared/Music/"
 video_path="/data/data/com.termux/files/home/storage/shared/Movies/"
+image_path="/data/data/com.termux/files/home/storage/pictures"
 
 find $dir -maxdepth 1 -type f \( -iname "*.pdf" -o -iname "*.pptx" -o -iname "*.epub" \) -exec mv {} $doc_path \;
 find $dir -maxdepth 1 -type f \( -iname "*.mp3" -o -iname "*.flac" -o -iname "*.wav" \) -exec mv {} $music_path \;
 find $dir -maxdepth 1 -type f \( -iname "*.mp4" -o -iname "*.mkv" \) -exec mv {} $video_path \;
+find $dir -maxdepth 1 -type f \( -iname "*.jpg" -o -iname "*.png" \) -exec mv {} $image_path \;
 echo Done
