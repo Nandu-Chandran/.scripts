458d785feaea24b46d5fcbde7d52ba739812a9db
Nandu-Chandran <nanduchandran121@gmail.com>
2022-05-26 03:16:44 +0530

    Thursday 26 May 2022 03:16:44 AM

---
+++ radio.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Dmenu script for editing some of my more frequently edited config files.
+
+
+declare -a options=("mango"
+"club"
+"mirchi"
+"pattupetti"
+"quit"
+)
+
+# The combination of echo and printf is done to add line breaks to the end of each
+# item in the array before it is piped into dmenu.  Otherwise, all the items are listed
+# as one long line (one item).
+
+choice=$(echo "$(printf '%s\n' "${options[@]}")" | dmenu -p 'Select a station: ')
+case "$choice" in
+	quit)
+		echo "Program terminated." && killall mpv 
+	;;
+	mango)
+		choice="https://bcovlive-a.akamaihd.net/19b535b7499a4719a5c19e043063f5d9/ap-southeast-1/6034685947001/playlist.m3u8"
+	;;
+	club)
+		choice="http://playerservices.streamtheworld.com/api/livestream-redirect/CLUBFMUAEAAC.aac"
+	;;
+	mirchi)
+		choice="https://n02.radiojar.com/q6hbcwmx8vzuv.mp3?rj-ttl=5&rj-tok=AAABdW3c0j8AU1QT19VJzvtLWw"
+	;;
+	pattupetti)
+		choice="https://listen.radioking.com/radio/305023/stream/354512"
+	;;
+	*)
+		exit 1
+	;;
+esac
+
+# Ultimately, what do want to do with our choice?  Open in our editor!
+killall mpv
+mpv "$choice" &
+# emacsclient -c -a emacs "$choice"
+
